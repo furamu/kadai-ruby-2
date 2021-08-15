44578a457fac303f8a375c940fb26e51833c23df
furamu <ganbabenkyo@yahoo.co.jp>
2021-08-15 12:55:42 +0000

    kadai-ruby-2

---
+++ kadai-fizzbuzz.rb
@@ -0,0 +1,25 @@
+def FizzBuzz(num)
+ 
+if num%3==0
+    return "Fizz"
+
+    
+elsif num%5==0
+    return  "Buzz"
+    
+elsif  (num%3==0) && (num%5==0)
+    return  "FizzBuzz"
+    
+else 
+    return num
+
+end
+end
+
+
+num_max = 100
+
+    
+(1..num_max).each do|number|
+ puts FizzBuzz(number)
+end
